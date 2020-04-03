83528663034d7d7b3f8738ebf77339cddc971edd
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2020-04-03 18:44:42 +0300

    Redisign and add telega notif

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -79,8 +79,8 @@ class Form extends Component {
 				res => {
                     if(res.status === 200) 
                         res.text()
-                        .then(text=>{
-                            console.log(text)
+                        .then(text => {console.log('text');this.TelegaSend()})
+                        .then(() =>{
                             this.setState({
                                 name: '',
                                 tel: '',
@@ -100,17 +100,33 @@ class Form extends Component {
                                 class: '',
                                 year: '',
                                 response: true
-                            });
-                        })
+                            })
+                        });
                     else this.setState({failresponse: true})
 			},  rej =>{
-				console.log("server Errror")
+                console.log("server Errror")
+                this.setState({failresponse: true})
 			});
 			
 		} catch (error) {
 			console.error('Ошибка:', error);
         }
         return false;
+    }
+    TelegaSend = () =>{
+        const token = process.env.REACT_APP_TELEGA_BOT_TOKEN;
+        const chat_id = "668582787";
+        const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=New user: Name: ${this.state.name}, Telephone: ${this.state.tel}.`
+        fetch(url, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, cors, *same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                    'Content-Type': 'application/json',
+                    //'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
     }
     render() {
         return (
@@ -164,14 +180,6 @@ class Form extends Component {
                             <option value="3">3</option>
                             <option value="4+">4+</option>
                         </select>
-                        {/* <select value={this.state.walls} name='walls' placeholder='Стены (цегла,газоблок,керамоблок,залізобетон,інше)' onChange={(e) => this.handleInputChange(e)}>
-                            <option value="">Стены</option>
-                            <option value="цегла">цегла</option>
-                            <option value="газоблок">газоблок</option>
-                            <option value="керамоблок">керамоблок</option>
-                            <option value="залізобетон">залізобетон</option>
-                            <option value="інше">інше</option>
-                        </select> */}
                         <input type="number" value={this.state.year} name='year' placeholder='Год постройки' onChange={(e) => this.handleInputChange(e)}/>
                         </div>
                         <div>
@@ -179,28 +187,22 @@ class Form extends Component {
                         <input type="number" value={this.state.sqarelive} required name='sqarelive' placeholder='Жилая пл. (кв. м)*' onChange={(e) => this.handleInputChange(e)}/>
                         </div>
                     </div>
-                    {/* <div className='third'>
-                        <input type="number" value={this.state.sqaremain} name='sqaremain' placeholder='Общая пл. (кв. м)*' onChange={(e) => this.handleInputChange(e)}/>
-                        <input type="number" value={this.state.sqarelive} name='sqarelive' placeholder='Жилая пл. (кв. м)*' onChange={(e) => this.handleInputChange(e)}/>
-                        <input type="number" value={this.state.sqarekit} name='sqarekit' placeholder='Кухня (кв. м)' onChange={(e) => this.handleInputChange(e)}/> 
-                    </div> */}
                     <div className='desc'>
                         <div>
                             <input type="number" value={this.state.floor} required name='floor' placeholder='Этаж*' onChange={(e) => this.handleInputChange(e)}/>
-                            <input type="number" value={this.state.flooring} name='flooring' placeholder='Этажность' onChange={(e) => this.handleInputChange(e)}/>
+                            <input type="number" value={this.state.price} name='price' placeholder='Цена' onChange={(e) => this.handleInputChange(e)}/>
                         </div>
                         <textarea rows = "3" cols = "60" value={this.state.description} name='description' placeholder='Описание объекта' onChange={(e) => this.handleInputChange(e)}/>
                     </div>
-                        {/* <input type="text" value={this.state.heating} name='heating' placeholder='Отопление' onChange={(e) => this.handleInputChange(e)} className='small'/> */}
-                        <select value={this.state.planning} name='planning' placeholder='Особенности планировки' onChange={(e) => this.handleInputChange(e)}>
-                            <option value="">Особенности планировки</option>
-                            <option value="черновая отделка">черновая отделка</option>
-                            <option value="без мебели">без мебели</option>
-                            <option value="пентхаус">пентхаус</option>
-                            <option value="многоуровневая с мансардой">многоуровневая с мансардой</option>
-                            <option value="кухня-студия">кухня-студия</option>
-                            <option value="без особенностей">без особенностей</option>
-                        </select>
+                    {/* <select value={this.state.planning} name='planning' placeholder='Особенности планировки' onChange={(e) => this.handleInputChange(e)}>
+                        <option value="">Особенности планировки</option>
+                        <option value="черновая отделка">черновая отделка</option>
+                        <option value="без мебели">без мебели</option>
+                        <option value="пентхаус">пентхаус</option>
+                        <option value="многоуровневая с мансардой">многоуровневая с мансардой</option>
+                        <option value="кухня-студия">кухня-студия</option>
+                        <option value="без особенностей">без особенностей</option>
+                    </select>
                     <div className='second'>
                         <select value={this.state.parking} name='parking' placeholder='Паркинг' onChange={(e) => this.handleInputChange(e)}>
                             <option value="">Паркинг</option>
@@ -212,7 +214,6 @@ class Form extends Component {
                             <option value="гаражные боксы">гаражные боксы</option>
                         </select>
                         <div className='fifth'>
-                            {/* <input type="text" value={this.state.warming} name='warming' placeholder='Утепление' onChange={(e) => this.handleInputChange(e)}/> */}
                             <input type="number" value={this.state.price} name='price' placeholder='Цена' onChange={(e) => this.handleInputChange(e)}/>
                             <select value={this.state.parking} name='currency' placeholder='Валюта' onChange={(e) => this.handleInputChange(e)}>
                                 <option value="">Валюта</option>
@@ -220,7 +221,6 @@ class Form extends Component {
                                 <option value="UAH">UAH</option>
                                 <option value="EUR">EUR</option>
                             </select>
-                            {/* <input type="text" value={this.state.currency} name='currency' placeholder='Валюта' onChange={(e) => this.handleInputChange(e)}/> */}
                         </div>
                     </div>
                     <div className='first'>
@@ -238,12 +238,14 @@ class Form extends Component {
                             <option value="Бизнес">Бизнес</option>
                             <option value="Элит">Элит</option>
                         </select>
-                    </div>
+                    </div> */}
                     <p>* - обязательные к заполнению поля</p>
-                    <button>Оформить заявку</button>
+                    {
+                        !(this.state.response || this.state.failresponse) ? <button>Получить предложение</button> : ''
+                    }
                 </form>
                 {
-                    this.state.response ? <h4>Успешно</h4> : ''
+                    this.state.response ? <h4>Заявка отправлена!</h4> : ''
                 }
                 {
                     this.state.failresponse ? <h4 className='h4error'>Что-то пошло не так, попробуйте ещё</h4> : ''
--- src/Components/MainSlider/MainSlider.jsx
+++ src/Components/MainSlider/MainSlider.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './MainSlider.scss';
-import {Link} from 'react-scroll';
+// import {Link} from 'react-scroll';
 
 
 class MainSlider extends Component {
@@ -9,7 +9,7 @@ class MainSlider extends Component {
             <div className='MainSlider' >
                 <h1>Выкуп недвижимости</h1>
                 <h2>Быстро выкупим вашу недвижимость по рыночной цене</h2>
-                <button><Link activeClass="active" to="form" spy={true} smooth={true} offset={50} duration={500}>продать недвижимость</Link></button>
+                {/* <button><Link activeClass="active" to="form" spy={true} smooth={true} offset={50} duration={500}>продать недвижимость</Link></button> */}
             </div>
         );
     }
